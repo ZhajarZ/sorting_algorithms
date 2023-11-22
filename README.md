05c40cd8ad93185245a93ad0529ec989381bbcfb
Hajar <hajar1445@gmail.com>
2023-11-21 17:19:46 -0800

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>0x1B. C - Sorting algorithms & BigO</h1>
---
+++ sort.h
@@ -0,0 +1,21 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define UP 0
+#define DOWN 1
+
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+void bubble_sort(int *array, size_t size);
+
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
